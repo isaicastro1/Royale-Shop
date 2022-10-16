973f604e40122348c27b280b31401df37dc4b8dc
Isai Castro <isaicstr@gmail.com>
2022-10-15 19:34:20 -0700

    Added button for user to sign in with Google redirect

--- src/routes/sign-in/sign-in.js
+++ src/routes/sign-in/sign-in.js
@@ -1,18 +1,34 @@
 import {
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
+  auth,
 } from "../../utils/firebase/firebase";
 
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
 const SignIn = () => {
+  useEffect(() => {
+    getRedirectResult(auth).then((response) => {
+      if (response) {
+        console.log(response);
+      }
+    });
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
-    createUserDocumentFromAuth(user);
+    const userDocRef = await createUserDocumentFromAuth(user);
   };
 
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign In with Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign In with Google Redirect
+      </button>
     </div>
   );
 };
