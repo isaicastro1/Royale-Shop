3f4fd7b93848393aa6682a3cfc4625205af004ab
Isai Castro <isaicstr@gmail.com>
2022-10-30 15:57:43 -0700

    Added feature to update shopping cart icon number based on items in cart

--- src/contexts/cart.context.js
+++ src/contexts/cart.context.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
   //find if cartItems array contains product to add
@@ -6,7 +6,8 @@ const addCartItem = (cartItems, productToAdd) => {
     (cartItem) => cartItem.id === productToAdd.id
   );
 
-  //if product is not in cartItems array, return new array with exisiting cart items plus new product with quantity
+  //if product is not in cartItems array, return new array with exisiting cart items
+  // plus new product with quantity
   if (exisitingCartItem) {
     return cartItems.map((cartItem) =>
       cartItem.id === productToAdd.id
@@ -24,17 +25,36 @@ export const CartContext = createContext({
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
+  cartCount: 0,
+  setCartCount: () => {},
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setcartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+
+  // Adds count to shopping cart icon
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity,
+      0
+    );
+    setCartCount(newCartCount);
+  }, [cartItems]);
 
   const addItemToCart = (productToAdd) => {
     setcartItems(addCartItem(cartItems, productToAdd));
   };
 
-  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    addItemToCart,
+    cartItems,
+    setCartCount,
+    cartCount,
+  };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
