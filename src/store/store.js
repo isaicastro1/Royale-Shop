313b2ca036db45960ab74afe8c3af6f151d15d34
Isai Castro <isaicstr@gmail.com>
2023-01-22 18:38:22 -0700

    Added Redux persist to persist reducers when page is refreshed

--- src/store/store.js
+++ src/store/store.js
@@ -5,6 +5,9 @@ import {
 } from "redux";
 // import logger from "redux-logger";
 
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import { rootReducer } from "./root-reducer";
 
 const middleWare = (store) => (next) => (action) => {
@@ -20,8 +23,22 @@ const middleWare = (store) => (next) => (action) => {
   // console.log("next state", store.getState());
 };
 
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middleWares = [middleWare];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
